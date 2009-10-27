0d9a9b1ff21083ebc8695ffbe7fddef8dcb5dd1a
NoHealing <NoHealing@341a3bae-4baa-4b23-84f6-8fab70f401ce>
2009-10-27 21:48:33 +0000

    Oups!

---
+++ patches/Mises à Jour/mob_db-rev566.sql
@@ -0,0 +1,5 @@
+UPDATE mob_db SET kName = "Garde en Chef" WHERE ID = 1981;
+UPDATE mob_db SET kName = "Orc Tirreur d'Elite" WHERE ID = 1982;
+UPDATE mob_db SET kName = "Esprit Dérangé d'Orc" WHERE ID = 1983;
+UPDATE mob_db SET kName = "Shaman Cargalache" WHERE ID = 1984;
+UPDATE mob_db SET kName = "Chapardeur Doré" WHERE ID = 1086;
