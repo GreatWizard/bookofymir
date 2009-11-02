b2810e7c27809a0f791b72bd00b93310a8b2dae0
NoHealing <NoHealing@341a3bae-4baa-4b23-84f6-8fab70f401ce>
2009-11-02 14:55:54 +0000

    * Ajout de la traduction de la Sanctuary Quest de quest_rachel. * Corrections sur cities/rachel. * Mise à jour d'halloween_2009. * Corrections diverses. * Correction d'une typo sur un certain nombre de mobs.

---
+++ patches/Mises à Jour/mob_db-rev587.sql
@@ -0,0 +1,5 @@
+UPDATE mob_db SET kName = "Cauchemar" WHERE ID = 1061;
+UPDATE mob_db SET kName = "Cauchemar Terrifiant" WHERE ID = 1379;
+UPDATE mob_db SET kName = "Cauchemar" WHERE ID = 1427;
+UPDATE mob_db SET kName = "Cauchemar Terrifiant" WHERE ID = 1554;
+UPDATE mob_db SET kName = "Cauchemar" WHERE ID = 1964;
